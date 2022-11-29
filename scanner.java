9a7734cb097b5b570a28dd98f813002ab149242d
JonlyTrix <janlukas113@gmail.com>
2022-11-29 15:26:25 +0100

    On branch LF05
     Changes to be committed:
    	modified:   scanner.java

--- scanner.java
+++ scanner.java
@@ -1,31 +1,25 @@
 import java.util.Scanner;// Um die Scanner-Klasse zu verwenden, muss diese importiert werden
 
-public class ScannerDemo {
+public class scanner {
 
     public static void main(String[] args) {
-
-        int ganzzahl=0;
-        double kommazahl=0.0;
-        String wort="";
-        boolean wahrheitswert=false;
-        char buchstabe='';
-
         Scanner tastatur = new Scanner(System.in); // Erzeugen eines Objekts der Klasse Scanner
+        System.out.println("Eine ganze Zahl");
+        int ganzzahl= tastatur.nextInt();
+        System.out.println("Eine  kommazahl");
+        double kommazahl= tastatur.nextDouble();
+        System.out.println("Ein Wort");
+        String wort= tastatur.next();
+        System.out.println("Ein Bool");
+        boolean wahrheitswert=tastatur.nextBoolean();
+        System.out.println("Ein Char");
+        char buchstabe=tastatur.next().charAt(0);
 
-        System.out.print("Bitte geben Sie eine ganze Zahl ein: ");
-        System.out.print("Sie haben " + ganzzahl + " eingegeben!\n\n");
-
-        System.out.print("Bitte geben Sie eine Zahl mit Nachkommastellen ein: ");
-        System.out.print("Sie haben " + kommazahl + " eingegeben!\n\n");
-
-        System.out.print("Bitte geben Sie eine Wort ein : ");
-        System.out.print("Sie haben '" + wort + "' eingegeben!\n\n");
-
-        System.out.print("Bitte geben Sie einen Wahrheitswert ein (true / false): ");
-        System.out.print("Sie haben '" + wahrheitswert + "' eingegeben!\n\n");
-
-        System.out.print("Bitte geben Sie einen einzelnen Buchstaben ein: ");
-        System.out.print("Sie haben '" + buchstabe + "' eingegeben!\n");
+        System.out.println("Sie haben " + ganzzahl + " eingegeben!\n\n");
+        System.out.println("Sie haben " + kommazahl + " eingegeben!\n\n");
+        System.out.println("Sie haben '" + wort + "' eingegeben!\n\n");
+        System.out.println("Sie haben '" + wahrheitswert + "' eingegeben!\n\n");
+        System.out.println("Sie haben '" + buchstabe + "' eingegeben!\n");
        
         tastatur.close();
     }
