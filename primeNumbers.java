a8276d5b4f337e9911d7ebc2ac8cf01253471995
JonlyTrix <janlukas113@gmail.com>
2022-12-05 15:37:50 +0100

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   primeNumbers.java

--- primeNumbers.java
+++ primeNumbers.java
@@ -1,29 +1,30 @@
 import java.util.Scanner;
 
 public class primeNumbers {
-    public static void main(String[] args) {
-        Scanner tastatur = new Scanner(System.in);
-        while(true){
-
-
-        
-        int x;
-        
-        System.out.print("Zahl:");
-        x = tastatur.nextInt();
-        for(int i=1; i<=x; i=i+1) {
+public static void main(String[] args) {
+   
+    while(true){
+    int x;
+    Scanner tastatur = new Scanner(System.in);
+    System.out.print("Zahl:");
+    x = tastatur.nextInt();
+    for(int i=1; i<=x; i=i+1) {
             
-            if(x%i==0 && i!=x&&i!=1 || x == 1){
-                System.out.println("keine Primzahl... Teilbar durch: " + i);
-                break;
+        if(x%i==0 && i!=x&&i!=1 || x == 1){
+            System.out.println("keine Primzahl... Teilbar durch: " + i);
+            tastatur.close();
+            break;
             }
-            else if(i/x==1){
-                System.out.println("ist ne Primzahl");
-                break;
+        else if(i/x==1){
+            System.out.println("ist ne Primzahl");
+            tastatur.close();
+            break;
                 
             }
         }
+       
     }
+    
     
 }   
 }
